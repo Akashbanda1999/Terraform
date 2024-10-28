c60700cbaf99fd55d8a0cc1c84af9395a9e09400
Akash Banda <akashbanda9@gmail.com>
2024-10-28 11:33:27 +0530

    pro

--- provider-block_and-resource-block/profile.tf
+++ provider-block_and-resource-block/profile.tf
@@ -1,5 +1,6 @@
 # profile.tf
 provider "aws" {
+  alias   = "test"
   region  = "us-east-1"              # Replace with your desired region
   profile = "configs"   # Replace with the AWS CLI profile name you want to use
 }
