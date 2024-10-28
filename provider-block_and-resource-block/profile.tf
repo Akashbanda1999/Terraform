699e9984844e1215ae6a6800eb79bf88932607b2
Akash Banda <akashbanda9@gmail.com>
2024-10-28 11:31:06 +0530

    pro

--- provider-block_and-resource-block/profile.tf
+++ provider-block_and-resource-block/profile.tf
@@ -1,4 +1,5 @@
+# profile.tf
 provider "aws" {
-    profile = "ec2"
+  region  = "us-east-1"              # Replace with your desired region
+  profile = "ec2"   # Replace with the AWS CLI profile name you want to use
 }
- 
