2a4c837ede89742d8276813e026bab181ed61c90
Akash Banda <akashbanda9@gmail.com>
2024-10-28 12:04:48 +0530

    false

--- provider-block_and-resource-block/ec2.tf
+++ provider-block_and-resource-block/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "this_ubuntu" {
 
     ami = "ami-0325498274077fac5" 
-    disable_api_stop  = true 
-    disable_api_termination = true  
+    disable_api_stop  = false
+    disable_api_termination = false  
     instance_type = "t4g.nano" 
     key_name = "north"
     count = 4  #loop 
