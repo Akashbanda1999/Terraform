91b57a2388986a7587a8b11599901344ad040088
Akash Banda <akashbanda9@gmail.com>
2024-10-28 11:15:44 +0530

    ec2

--- provider-block_and-resource-block/ec2.tf
+++ provider-block_and-resource-block/ec2.tf
@@ -4,8 +4,8 @@ resource "aws_instance" "this_ubuntu" {
     disable_api_stop  = true 
     disable_api_termination = true  
     instance_type = "t2.micro" 
-    key_name = "mumbaianupb38kp"
-    security_groups = ["sg-0206fe395dc0f6d38"]
+    key_name = "north"
+    security_groups = ["sg-02d0271c0935355dc"]
     count = 4  #loop 
     tags = {
       purpose = "jenkins server"
