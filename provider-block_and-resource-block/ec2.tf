4efb73da82202411a6c3a82059e8dc4fb3789cc5
Akash Banda <akashbanda9@gmail.com>
2024-10-28 11:48:53 +0530

    sg

--- provider-block_and-resource-block/ec2.tf
+++ provider-block_and-resource-block/ec2.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "this_ubuntu" {
     disable_api_termination = true  
     instance_type = "t4g.nano" 
     key_name = "north"
-    security_groups = ["sg-02d0271c0935355dc"]
+    vpc_security_group_ids = ["vpc-0672e3c1babbc8b3c"]
     count = 4  #loop 
     tags = {
       purpose = "ec2"
