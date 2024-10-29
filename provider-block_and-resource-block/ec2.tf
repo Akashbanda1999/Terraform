0e4d38bbb8f970d5e92a5ebc3c331af253e76e75
Akash Banda <akashbanda9@gmail.com>
2024-10-29 09:45:57 +0530

    Update ec2.tf

--- provider-block_and-resource-block/ec2.tf
+++ provider-block_and-resource-block/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "this_ubuntu" {
     ami = "ami-0325498274077fac5" 
     disable_api_stop  = false
     disable_api_termination = false  
-    instance_type = "t4g.nano" 
+    instance_type = "t2.micro" 
     key_name = "north"
     count = 4  #loop 
     tags = {
