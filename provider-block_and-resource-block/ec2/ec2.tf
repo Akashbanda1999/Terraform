83b979e3c7616aa8769d81c97e0bff2e04861586
Akash Banda <akashbanda9@gmail.com>
2024-11-12 12:14:11 +0530

    ec2

--- provider-block_and-resource-block/ec2/ec2.tf
+++ provider-block_and-resource-block/ec2/ec2.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "this_ubuntu" {
 
-    ami =  var.my-ami
+    ami = var.my-ami
     disable_api_stop  = var.my-disable_api_stop
-    disable_api_termination =var.my-disable_api_termination
-    instance-type =var.my-instance-type
-    key_name =var.my-key
-    count =var.my-count  #loop 
+    disable_api_termination = var.my-disable_api_termination
+    instance-type = var.my-instance-type
+    key_name = var.my-key
+    count = var.my-count  #loop 
     tags = {
       purpose = "ec2"
     } 
