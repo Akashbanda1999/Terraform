65af9c1a8c2df88f8a95b343e5da8b9ff93d08ae
Akash Banda <akashbanda9@gmail.com>
2024-11-21 14:18:42 +0530

    Update main.tf

--- provider-block_and-resource-block/DynamoDB/main.tf
+++ provider-block_and-resource-block/DynamoDB/main.tf
@@ -1,9 +1,9 @@
 resource "aws_dynamodb_table" "my_app_table" {
     name = "test-my-app-table-d"
     billing_mode = "PAY_PER_REQUEST"
-    hash_key = "id"
+    hash_key = "LockID"
     attribute {
-        name = "id"
+        name = "LockID"
         type = "S"
     }
 }
