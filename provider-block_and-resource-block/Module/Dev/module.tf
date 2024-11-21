ad1d0ba45283ae0e392295815d16583954986f8d
Akash Banda <akashbanda9@gmail.com>
2024-11-21 11:26:30 +0530

    Update module.tf

--- provider-block_and-resource-block/Module/Dev/module.tf
+++ provider-block_and-resource-block/Module/Dev/module.tf
@@ -1,11 +1,11 @@
 
 module "ec2" {
     source = "/home/anup/terraform/deleteafterpractice/Module/Resources/EC2"
-    this_image_id = "ami-012967cc5a8c9f891"
+    this_image_id = "ami-0866a3c8686eaeeba"
     this_list = "t2.micro"
     this_disable_api_stop = false
     this_disable_api_termination = false
-    #this_vpc_security_group_ids = "sg-0505874879e7ce6cd"
+    #this_vpc_security_group_ids = "sg-02d0271c0935355dc"
     this_aws_instance_subnet = module.vpc.subnet_id
     #this_sg_vpc_id = module.vpc.vpc_id
     //this_aws_vpc_id = module.aws_vpc.aws_vpc_id
