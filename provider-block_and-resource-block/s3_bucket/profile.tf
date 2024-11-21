368c4b4093b7368f2d70052b07733c6f1d65d171
Akash Banda <akashbanda9@gmail.com>
2024-11-21 12:09:52 +0530

    Update profile.tf

--- provider-block_and-resource-block/s3_bucket/profile.tf
+++ provider-block_and-resource-block/s3_bucket/profile.tf
@@ -1,5 +1,4 @@
 provider "aws" {
- region = "us-east-1"
- profile = "configs"
- 
+  region  = "us-east-1"              # Replace with your desired region
+  profile = "configs"   # Replace with the AWS CLI profile name you want to use
 }
