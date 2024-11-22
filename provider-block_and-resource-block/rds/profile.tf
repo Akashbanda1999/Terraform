8a94b996bed0f8fe0804f25454d625e2db893b07
Akash Banda <akashbanda9@gmail.com>
2024-11-23 01:23:05 +0530

    Update profile.tf

--- provider-block_and-resource-block/rds/profile.tf
+++ provider-block_and-resource-block/rds/profile.tf
@@ -1,6 +1,5 @@
 # profile.tf
 provider "aws" {
-  alias   = "test"
   region  = "us-east-1"              # Replace with your desired region
   profile = "configs"   # Replace with the AWS CLI profile name you want to use
 }
