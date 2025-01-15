1f3757f37651d066683f3117833a45814216c89b
Akash Banda <akashbanda9@gmail.com>
2025-01-15 12:27:24 +0530

    del

--- provider-block_and-resource-block/EKS/variable.tf
+++ provider-block_and-resource-block/EKS/variable.tf
@@ -1,4 +1,4 @@
-variable "region" {
+/*variable "region" {
   default = "us-east-1"
 }
 
@@ -15,4 +15,4 @@ variable "subnets" {
   description = "The list of subnets for the EKS cluster"
   type        = list(string)
   default     = ["subnet-0c0ee504b24edba00","subnet-08d9250923457ef56","subnet-07cc631e7b8ef1d7f","subnet-0453783e8ac0b07f3","subnet-06299bc9fc7ea8f51"]
-}
+}*/
