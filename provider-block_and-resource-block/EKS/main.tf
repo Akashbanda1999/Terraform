0d658703b3b0192bdd445be85f6bc189f073a6d6
Akash Banda <akashbanda9@gmail.com>
2025-01-15 12:15:16 +0530

    Update main.tf

--- provider-block_and-resource-block/EKS/main.tf
+++ provider-block_and-resource-block/EKS/main.tf
@@ -3,11 +3,11 @@ module "eks" {
   version = "~> 19.0"
 
   cluster_name    = "my-eks-cluster"
-  node_group_name = "node"
   cluster_version = "1.31" # Latest EKS version at the time of writing
   subnet_ids      = ["subnet-0c0ee504b24edba00","subnet-08d9250923457ef56","subnet-07cc631e7b8ef1d7f","subnet-0453783e8ac0b07f3","subnet-06299bc9fc7ea8f51"]
   vpc_id          = "vpc-0f8bace96ea512f5b"
 
+  node_group_name = "node"
   scaling_config = {
     eks_nodes = {
       desired_capacity = 2
