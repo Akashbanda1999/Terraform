dd95294ea4fc8ae835fff88d44c603871f912da1
Akash Banda <akashbanda9@gmail.com>
2025-01-15 12:26:20 +0530

    eks

---
+++ provider-block_and-resource-block/EKS/IAM-Role.tf
@@ -0,0 +1,9 @@
+module "eks_roles" {
+  source  = "terraform-aws-modules/iam/aws//modules/eks"
+
+  eks_cluster_role_name = "eks-cluster-role"
+  eks_cluster_role_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
+    "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  ]
+}
