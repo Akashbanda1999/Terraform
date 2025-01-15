703929afe517850de83e51625c454647e5000d1c
Akash Banda <akashbanda9@gmail.com>
2025-01-15 15:39:52 +0530

    Update node-group.tf

--- provider-block_and-resource-block/EKS/node-group.tf
+++ provider-block_and-resource-block/EKS/node-group.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_role" "eks_node_role" {
   name = "eks-node-role"
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "latest"
     Statement = [
       {
         Action    = "sts:AssumeRole"
